08b5d9e747d7781ef2796b7c09f9a5109ca9a3da
lamp <virtual@riseup.net>
2025-10-24 15:47:59 +0200

    Add oldchunks module

--- src/main/java/com/github/thelampgod/worldbadger/modules/ModuleManager.java
+++ src/main/java/com/github/thelampgod/worldbadger/modules/ModuleManager.java
@@ -3,6 +3,7 @@ package com.github.thelampgod.worldbadger.modules;
 import com.github.thelampgod.worldbadger.WorldBadger;
 import com.github.thelampgod.worldbadger.modules.impl.BlockModule;
 import com.github.thelampgod.worldbadger.modules.impl.EntityModule;
+import com.github.thelampgod.worldbadger.modules.impl.OldChunksModule;
 import com.github.thelampgod.worldbadger.modules.impl.SignModule;
 import net.querz.mca.Chunk;
 import net.querz.nbt.CompoundTag;
@@ -22,6 +23,7 @@ public class ModuleManager {
         modules.add(new SignModule());
         modules.add(new BlockModule());
         modules.add(new EntityModule());
+        modules.add(new OldChunksModule());
     }
 
     public Optional<SearchModule> findModule(String name) {
---
+++ src/main/java/com/github/thelampgod/worldbadger/modules/impl/OldChunksModule.java
@@ -0,0 +1,107 @@
+package com.github.thelampgod.worldbadger.modules.impl;
+
+import com.github.thelampgod.worldbadger.modules.SearchModule;
+import com.github.thelampgod.worldbadger.output.DataClass;
+import lombok.Data;
+import net.querz.mca.Chunk;
+import net.querz.nbt.CompoundTag;
+import net.querz.nbt.ListTag;
+import net.querz.nbt.Tag;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class OldChunksModule extends SearchModule {
+    private final Set<String> shitStones = Set.of("minecraft:andesite", "minecraft:diorite", "minecraft:granite");
+
+    public OldChunksModule() {
+        super("oldchunks");
+    }
+
+    @Override
+    public List<? extends DataClass> processChunk(Chunk chunk) {
+        boolean isNewerThan1_7 = false;
+        final int x = chunk.getX();
+        final int z = chunk.getZ();
+
+        //check for existence of shitstones
+        for (int i = 0; i < 5; ++i) {
+            CompoundTag section = chunk.getSection(i);
+            if (section == null) continue;
+
+            CompoundTag blockStates = section.getCompound("block_states");
+            Set<String> blockIds = getPaletteBlockIds(blockStates.getList("palette"));
+
+
+            for (String stone : shitStones) {
+                if (blockIds.contains(stone)) {
+                    isNewerThan1_7 = true;
+                    break;
+                }
+            }
+            // no need to check any more section
+            if (isNewerThan1_7) break;
+        }
+
+        // if option "all", return a ChunkData result with state of chunk boolean
+        if (!idToOptionsMap.get("all").isEmpty()) {
+            return List.of(new ChunkData(x, z, isNewerThan1_7));
+        }
+
+        // otherwise, only return oldchunks positions
+        return (isNewerThan1_7) ? null : List.of(new OldChunkData(x, z));
+    }
+
+    private Set<String> getPaletteBlockIds(ListTag palette) {
+        Set<String> blockIds = new HashSet<>();
+        for (Tag tag : palette) {
+            String blockId = ((CompoundTag) tag).getString("Name");
+            blockIds.add(blockId);
+        }
+        return blockIds;
+    }
+
+    @Override
+    public List<String> getValidOptions() {
+        return List.of("all"); //return both oldchunks and newchunks data
+    }
+
+    @Override
+    public boolean requiresId() {
+        return false;
+    }
+
+    @Data
+    private static class ChunkData implements DataClass {
+        private final int x;
+        private final int z;
+        private final boolean isNewerThan1_7;
+
+        @Override
+        public List<String> getFieldNames() {
+            return List.of("x", "z", "isNewerThan1_7");
+        }
+
+        @Override
+        public List<Object> getFieldValues() {
+            return List.of(x, z, isNewerThan1_7);
+        }
+    }
+
+    @Data
+    private static class OldChunkData implements DataClass {
+        private final int x;
+        private final int z;
+
+        @Override
+        public List<String> getFieldNames() {
+            return List.of("x", "z");
+        }
+
+        @Override
+        public List<Object> getFieldValues() {
+            return List.of(x, z);
+        }
+    }
+}
